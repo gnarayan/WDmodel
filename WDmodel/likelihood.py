a9ac75b09549a6bb4647902d1a0c045ff3de8613
Gautham Narayan <gsnarayan@gmail.com>
2017-02-12 20:28:28 -0700

    Return to likelihood for full spectrum

--- WDmodel/likelihood.py
+++ WDmodel/likelihood.py
@@ -16,21 +16,18 @@ def lnprob(theta, wave, model, kernel, balmer):
     return lp + lnlike(theta, wave, model, kernel, balmer)
 
 
-def lnlike(theta, wave, model, kernel, balmerlinedata):
-    teff, logg, av  = theta
+def lnlike(theta, spec, model, kernel):
+    teff, logg, av, c  = theta
 
-    mod = model._get_red_model(teff, logg, av, wave)
+    mod = model._get_red_model(teff, logg, av, spec.wave)
 
     # smooth the model, and extract the section that overlays the model
     # since we smooth the full model, computed on the full wavelength range of the spectrum
     # and then extract the subset range that overlaps with the data
     # we avoid any edge effects with smoothing at the end of the range
-    smoothedmod = convolve(mod, kernel)
-    (line_wave, line_flux, line_fluxerr, line_number, line_cflux, line_cov, line_ind, save_ind, mu, cov) = balmerlinedata
-
-    smoothedfn*=(line_cflux/mod_cflux)
-    #return gp.lnlikelihood(datafn - smoothedfn)
-    return -0.5*np.sum(((line_flux-smoothedfn)/line_fluxerr)**2.)
+    smoothedmod = convolve(mod, kernel, boundary='extend')
+    smoothedmod *= c
+    return -0.5*np.sum(((spec.flux-smoothedmod)/spec.flux_err)**2.)
 
 
 def nll(*args):
