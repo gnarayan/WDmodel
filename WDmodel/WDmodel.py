bf351fb00284471fcd13d6f826b07a1f48000eab
Gautham Narayan <gsnarayan@gmail.com>
2017-03-07 16:09:43 -0700

    Get rid of get_model_mags since we move to pbmodel, add _get_full_obs_model for convenience, add some docstrings

--- WDmodel/WDmodel.py
+++ WDmodel/WDmodel.py
@@ -2,8 +2,7 @@ import warnings
 import numpy as np
 from . import io
 import scipy.interpolate as spinterp
-import pysynphot as S
-from astropy import units as u                                                                                          
+from astropy import units as u
 from specutils.extinction import reddening
 from scipy.ndimage.filters import gaussian_filter
 
@@ -31,7 +30,7 @@ class WDmodel(object):
         D       = [ 130.0  ,  170.0  ,  125.0   ,  75.0   ,  50.0    ,  27.0   ]
         eps     = [  10.0  ,   10.0  ,   10.0   ,   8.0   ,   5.0    ,   3.0   ]
         self._lines = dict(zip(lno, zip(lines, H, D, eps)))
-        # we're passing grid_file so we know which model to init 
+        # we're passing grid_file so we know which model to init
         self._fwhm_to_sigma = np.sqrt(8.*np.log(2.))
         self.__init__tlusty(grid_file=grid_file)
 
@@ -44,13 +43,13 @@ class WDmodel(object):
 
         ingrid = io.read_model_grid(grid_file, grid_name)
         self._grid_file, self._grid_name, self._wave, self._ggrid, self._tgrid, self._flux = ingrid
-        
+
         # pre-init the interpolation and do it in log-space
         # note that we do the interpolation in log-log
         # this is because the profiles are linear, redward of the Balmer break in log-log
         # and the regular grid interpolator is just doing linear interpolation under the hood
         self._model = spinterp.RegularGridInterpolator((np.log10(self._wave), self._ggrid, self._tgrid),\
-                np.log10(self._flux))        
+                np.log10(self._flux))
 
 
     def _get_xi(self, teff, logg, wave):
@@ -64,12 +63,20 @@ class WDmodel(object):
 
 
     def _get_model(self, xi, log=False):
+        """
+        Returns the model flux given the xi (log wavelength, logg, temperature)
+        vector, such as that produced by _get_xi()
+        """
         if log:
             return self._model(xi)
         return (10.**self._model(xi))
 
 
     def _get_red_model(self, teff, logg, av, wave, rv=3.1, log=False, rvmodel='od94'):
+        """
+        Returns the reddened model flux given teff, logg, av, rv, rvmodel, and
+        wavelengths
+        """
         xi = self._get_xi(teff, logg, wave)
         mod = self._get_model(xi, log=log)
         bluening = reddening(wave*u.Angstrom, av, r_v=rv, model=rvmodel)
@@ -82,42 +89,41 @@ class WDmodel(object):
 
 
     def _get_obs_model(self, teff, logg, av, fwhm, wave, rv=3.1, log=False, rvmodel='od94'):
+        """
+        Returns the observed model flux given teff, logg, av, rv, rvmodel, fwhm
+        (for Gaussian instrumental broadening) and wavelengths
+        """
         xi = self._get_xi(teff, logg, wave)
         mod = self._get_model(xi, log=log)
         bluening = reddening(wave*u.Angstrom, av, r_v=rv, model=rvmodel)
         if log:
             mod = 10.**mod
         mod/=bluening
-        gsig = fwhm/self._fwhm_to_sigma
+        gsig = fwhm/self._fwhm_to_sigma * (1./np.median(np.gradient(wave)))
         mod = gaussian_filter(mod, gsig, order=0, mode='nearest')
         if log:
             mod = np.log10(mod)
         return mod
 
 
-    def get_model_mags(self, teff, logg, av, pbnames, pbmodel, mu=0., rv=3.1, rvmodel='od94'):
+    def _get_full_obs_model(self, teff, logg, av, fwhm, wave, rv=3.1, log=False, rvmodel='od94'):
         """
-        Returns a set of model magnitudes at teff, logg, av, rv, for rvmodel
-        through passbands pbnames with models pbmodels and zeropoint mu
-
-        Accepts:
-            teff: temp
-            logg: surface gravity
-            av: extinction
-            pbnames: iterable with names of passbands
-            pbmodel: dictionary with pysynphot ObsBandpass models for passbands in pbnames
-                If a passband is missing, will raise KeyError
-            mu: model mag zeropoint 
-            rv: reddening law slope (default=3.1)
-            rvmodel: model for reddening law (default='od94')
-
-        Returns a numpy array with model mags
+        Convenience function that does the same thing as _get_obs_model, but
+        also returns the full SED without any instrumental broadening applied
         """
-        mod = self._get_red_model(teff, logg, av, self._wave, rv=rv, log=False, rvmodel=rvmodel)
-        mod = S.ArraySpectrum(self._wave, mod, waveunits='angstrom', fluxunits='Flam')
-        out = [S.Observation(mod, pbmodel[pb]).effstim('vegamag') for pb in pbnames]
-        out = np.array(out) + mu
-        return out
+        xi = self._get_xi(teff, logg, self._wave)
+        mod = self._get_model(xi, log=log)
+        bluening = reddening(self._wave*u.Angstrom, av, r_v=rv, model=rvmodel)
+        if log:
+            mod = 10.**mod
+        mod/=bluening
+        omod = np.interp(wave, self._wave, mod)
+        gsig = fwhm/self._fwhm_to_sigma * (1./np.median(np.gradient(wave)))
+        omod = gaussian_filter(omod, gsig, order=0, mode='nearest')
+        if log:
+            omod = np.log10(omod)
+            mod  = np.log10(mod)
+        return omod, mod
 
 
     @classmethod
@@ -134,14 +140,14 @@ class WDmodel(object):
             raise ValueError(message)
 
         if len(wave) == 1:
-            return 
+            return
 
         dwave = np.diff(wave)
         if not(np.all(dwave > 0) or np.all(dwave < 0)):
             message = 'Wavelength array is not monotonic'
             raise ValueError(message)
 
-        
+
     def get_model(self, teff, logg, wave=None, log=False, strict=True):
         """
         Returns the model (wavelength and flux) for some teff, logg at wavelengths wave
@@ -185,8 +191,8 @@ class WDmodel(object):
         else:
             message = 'No valid wavelengths'
             raise ValueError(message)
-            
-            
+
+
     def get_red_model(self, teff, logg, av, rv=3.1, rvmodel='od94', wave=None, log=False, strict=True):
         """
         Returns the model (wavelength and flux) for some teff, logg av, rv with
@@ -209,7 +215,7 @@ class WDmodel(object):
             modflux = np.log10(modflux)
         return modwave, modflux
 
-    
+
     def get_obs_model(self, teff, logg, av, fwhm, rv=3.1, rvmodel='od94', wave=None, log=False, strict=True):
         """
         Returns the model (wavelength and flux) for some teff, logg av, rv with
@@ -225,7 +231,7 @@ class WDmodel(object):
                 wave=wave, log=log, strict=strict)
         if log:
             modflux = 10.**modflux
-        gsig     = fwhm/np.sqrt(8.*np.log(2.))
+        gsig = fwhm/self._fwhm_to_sigma * (1./np.median(np.gradient(wave)))
         modflux = gaussian_filter(modflux, gsig, order=0, mode='nearest')
         if log:
             modflux = np.log10(modflux)
@@ -244,7 +250,7 @@ class WDmodel(object):
         ind = np.where((self._wave >= wave.min()) & (self._wave <= wave.max()))[0]
         modelmedian = np.median(self._model.values[ind,:,:])
         if not log:
-            modelmedian = 10.**modelmedian    
+            modelmedian = 10.**modelmedian
         datamedian  = np.median(flux)
         self._fluxnorm = modelmedian/datamedian
         return self._fluxnorm
@@ -252,6 +258,10 @@ class WDmodel(object):
 
     @classmethod
     def _get_indices_in_range(cls, w, WA, WB, W0=None):
+        """
+        Accepts a wavelength array, and blue and redlimits, and returns the
+        indices in the array that are between the limits
+        """
         if W0 is None:
             W0 = WA + (WB-WA)/2.
         ZE  = np.where((w >= WA) & (w <= WB))
@@ -260,7 +270,7 @@ class WDmodel(object):
 
     def _get_line_indices(self, w, line):
         """
-        Returns the central wavelength, and _indices_ of the line profile 
+        Returns the central wavelength, and _indices_ of the line profile
         The widths of the profile are predefined in the _lines attribute
         """
         _, W0, WID, DW = self._lines[line]
@@ -279,7 +289,7 @@ class WDmodel(object):
             return w[ZE], f[ZE]
         else:
             return w[ZE], f[ZE], df[ZE]
-        
+
 
 
     def extract_spectral_line(self, w, f, line, df=None):
@@ -292,7 +302,7 @@ class WDmodel(object):
         try:
             _, W0, WID, DW = self._lines[line]
         except KeyError:
-            message = 'Line name %s is not valid. Must be one of (%s)' %(str(line), ','.join(self._lines.keys())) 
+            message = 'Line name %s is not valid. Must be one of (%s)' %(str(line), ','.join(self._lines.keys()))
             raise ValueError(message)
 
         w  = np.atleast_1d(w)
@@ -315,7 +325,7 @@ class WDmodel(object):
     def _extract_spectral_line(self, w, f, line, df=None):
         """
         Same as extract_spectral_line() except no testing
-        Used internally to extract the spectral line for the model 
+        Used internally to extract the spectral line for the model
         """
         W0, ZE = self._get_line_indices(w,  line)
         return self._extract_from_indices(w, f, ZE, df=df)
@@ -332,5 +342,5 @@ class WDmodel(object):
 
 
     __call__ = get_model
-        
+
 
