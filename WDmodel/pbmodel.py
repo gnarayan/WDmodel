2e039bed9869fe72f04f9d6951b884790fc4e16b
Gautham Narayan <gsnarayan@gmail.com>
2017-03-08 17:50:34 -0700

    Add method to get synthetic mags, refractor get_pbmodel

--- WDmodel/pbmodel.py
+++ WDmodel/pbmodel.py
@@ -10,7 +10,7 @@ def synflux(spec, ind, pb):
         spec: recarray spectrum (wave, flux) - assumed to be Flam
         ind : indices of spectrum that overlap with the passband pb - assumed
         to be photon transmission
-        pb: the passband transmission 
+        pb: the passband transmission
 
     Returns the normalized flux of the spectrum through the passband
     """
@@ -27,34 +27,83 @@ def synphot(spec, ind, pb, zp=0.):
         spec: recarray spectrum (wave, flux) - assumed to be Flam
         ind : indices of spectrum that overlap with the passband pb - assumed
         to be photon transmission
-        pb: the passband transmission 
+        pb: the passband transmission
         zp: the zeropoint of the passband that puts the synthetic magnitude on
         some standard system
 
     Returns the normalized flux of the spectrum through the passband
-
     """
     flux = synflux(spec, ind, pb)
     m = -2.5*np.log10(flux) + zp
     return m
 
 
-def chop_syn_spec_pb(spec, pb, model):
+def get_model_synmags(model_spec, pbs, mu=0.):
+    """
+    Computes the synthetic magnitudes of SED spec through the passbands in
+    dictionary pbmodel, and optionally applies an offset, mu
+
+    Accepts
+        model_spec: model recarray spectrum (wave, flux) - assumed to be Flam
+        pbs: dictionary with pbname as key containing as a tuple:
+            passband: recarray of non-zero passband transmission (wave,
+            throughput)
+            transmission: the non-zero passband transmission interpolated onto
+            overlapping model wavelengths
+            ind: indices of model_spec wavelength that overlap with this passband
+            zp: zeropoint of this passband
+            (this structure can be created by get_pbmodel)
+        mu: offset to apply to the photometry (default=0)
+    Returns
+        recarray of synthetic magnitudes (pb, mag)
+    """
+    outpb  = []
+    outmag = []
+    for pbname, pbdata in pbs.items():
+        _, transmission, ind, zp = pbdata
+        pbsynmag = synphot(model_spec, ind, transmission, zp) + mu
+        outmag.append(pbsynmag)
+        outpb.append(pbname)
+    out = np.rec.fromarrays((outpb, outmag), names='pb,mag')
+    return out
+
+
+def interp_passband(wave, pb, model):
+    """
+    Figures out the indices of the wavelength array wave, that overlap with the
+    recarray passband pb (wave, throughput), interpolates the passband onto
+    overlapping wavelength array, padding out of range values with zero, and
+    returns the interpolated transmission, and the indices of the wavelength
+    array that overlap with the passband
+    """
+    _, ind = model._get_indices_in_range(wave, pb.wave.min(), pb.wave.max())
+    transmission = np.interp(wave[ind], pb.wave, pb.throughput, left=0., right=0.)
+    return transmission, ind
+
+
+def chop_syn_spec_pb(spec, model_mag, pb, model):
+    """
+    Trims the pysynphot bandpass pb to non-zero throughput,
+
+    Computes the zeropoint of the passband given the SED spec, and model
+    magnitude of spec in the passband
+
+    Returns the trimmed passband and its zeropoint
+    """
 
     # don't bother about zero padding
     mask = np.nonzero(pb.throughput)
     pwave = pb.wave[mask]
     ptput = pb.throughput[mask]
-
-    # trim the spectrum
-    _, ind = model._get_indices_in_range(spec.wave, pwave.min(), pwave.max())
-    swave = spec.wave
-    sflux = spec.flux
-
-    # return pb, spectrum
     outpb = np.rec.fromarrays((pwave, ptput), names='wave,throughput')
-    outspec = np.rec.fromarrays((swave, sflux), names='wave,flux')
-    return outpb, outspec, ind
+
+    # interpolate the transmission onto the overlapping wavelengths of the spectrum
+    transmission, ind = interp_passband(spec.wave, outpb, model)
+
+    # compute the zeropoint
+    outzp = model_mag - synphot(spec, ind, transmission)
+
+    return outpb, outzp
 
 
 def get_pbmodel(pbnames, model, pbfile=None):
@@ -74,10 +123,10 @@ def get_pbmodel(pbnames, model, pbfile=None):
 
     If there is no entry in pbfile for a passband, then we attempt to use the
     passband name as obsmode string as is. Loads the bandpasses corresponding
-    to each obsmode.  
-    
+    to each obsmode.
+
     Raises RuntimeError if a bandpass cannot be loaded.
-    
+
     Trims the bandpass to entries with non-zero transmission
     Determines the Vegamag zeropoint for the passband (i.e. zp that gives
     Vega=0. in all passbands)
@@ -119,18 +168,12 @@ def get_pbmodel(pbnames, model, pbfile=None):
         synphot_mag = ob.effstim(mag_type)
 
         # cut the passband to non-zero values and interpolate onto overlapping Vega wavelengths
-        cutpb, spec, cutind = chop_syn_spec_pb(vega, bp, model)
+        outpb, outzp = chop_syn_spec_pb(vega, synphot_mag, bp, model)
 
         # interpolate the passband onto the standard's  wavelengths
-        transmission = np.interp(spec.wave[cutind], cutpb.wave, cutpb.throughput, left=0., right=0.)
-
-        # set the zeropoint for the cut passband
-        thiszp = synphot_mag - synphot(spec, cutind, transmission)
-
-        # TODO get the model wavelengths and chop and interpolate transmission and cutind to that
-        _, ind = model._get_indices_in_range(model._wave, cutpb.wave.min(), cutpb.wave.max())
-        transmission = np.interp(model._wave[ind], cutpb.wave, cutpb.throughput, left=0., right=0.)
+        transmission, ind = interp_passband(model._wave, outpb, model)
 
         # save everything we need for this passband
-        out[pb] = (cutpb, transmission, ind, thiszp)
+        out[pb] = (outpb, transmission, ind, outzp)
+
     return out
