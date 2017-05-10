2d085c539e1143e2bff4a1614eb2207799c99daa
Gautham Narayan <gsnarayan@gmail.com>
2017-05-09 19:50:02 -0700

    Explictly add the numpy include dir

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from distutils.core import setup
 from Cython.Build import cythonize
+import numpy
 setup(
-    ext_modules = cythonize("WDmodel/_interpolate3d.pyx")
+    ext_modules = cythonize("WDmodel/_interpolate3d.pyx"),
+    include_dirs=[numpy.get_include()]
     )
