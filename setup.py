3b951b01f78841e749d4c1fac3070bc0d2b72ec9
Gautham Narayan <gsnarayan@gmail.com>
2017-05-09 21:50:37 -0700

    Remove setup.py since we don't use _interpolate3d

--- setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-import numpy
-setup(
-    ext_modules = cythonize("WDmodel/_interpolate3d.pyx"),
-    include_dirs=[numpy.get_include()]
-    )
