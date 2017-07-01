0da4b66119f853b4029d3b2f94d246d04b8ded68
Gautham Narayan <gsnarayan@gmail.com>
2017-07-01 12:14:40 -0400

    Add cache PYSYN_CDBS for photometry test, expand tests

--- test.sh
+++ test.sh
@@ -9,7 +9,8 @@ else
     ARUNNER="python"
 fi
 
-mpirun -np 4 $RUNNER -m WDmodel --specfile WDmodel/tests/test.flm --ignorephot --rebin 2 --everyn 3 --nprod 100 --mpi --redo 
+mpirun -np 4 $RUNNER -m WDmodel --specfile WDmodel/tests/test.flm --photfile WDmodel/tests/test.phot --rebin 2 --everyn 3 --nprod 100 --mpi --redo --trimspec 3700 5200 --samptype pt --ntemps 2 --nburnin 20 --nprod 100 --nwalkers 100 --covtype SHO --tau_fix True --tau 5000 --lamshift 2 --vel -50 --reddeningmodel f99 --excludepb F160W --rescale --blotch --phot_dispersion 0.001 --thin 2 --skipminuit --dl 450 --trimspec 3700 5200
+mpirun -np 4 $RUNNER -m WDmodel --specfile WDmodel/tests/test.flm --ignorephot --rebin 2 --everyn 3 --nprod 100 --mpi --redo --nwalkers 100
 if [ "$1" = -c ]; then
     coverage combine
 fi
