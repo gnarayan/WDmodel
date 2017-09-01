eea7b18952944b651b5c99577d5729ca94e0fb8e
Gautham Narayan <gsnarayan@gmail.com>
2017-09-01 16:35:44 -0400

    update email

--- WDmodel/__init__.py
+++ WDmodel/__init__.py
@@ -6,7 +6,7 @@ photometry to establish spectrophotometric standards
 
 from __future__ import unicode_literals
 __author__     = 'Gautham Narayan'
-__email__      = 'gnarayan@noao.edu'
+__email__      = 'gsnarayan@gmail.com'
 __maintainer__ = 'Gautham Narayan'
 __license__    = 'GPLv3'
 __version__    = '0.3.1'
