f70d1b7a132d14ec176cf4f8199670280f8dbbd4
Gautham Narayan <gsnarayan@gmail.com>
2017-09-05 17:10:13 -0400

    add some commentary and a legend with mean, stdev

--- data/photometry/20170731/compare_old_new_apparent_mags.py
+++ data/photometry/20170731/compare_old_new_apparent_mags.py
@@ -12,33 +12,40 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 
 def main():
+    # setup the old and new photometry files
     old_file = '../20170301/AC/WDphot_C22.dat'
     new_file = 'Combined/WDphot_C22_AC_AS_combined.dat'
 
+    # read the data
     old = at.Table.read(old_file, format='ascii')
     new = at.Table.read(new_file, format='ascii')
+
+    # determine available passbands and setup plot colors
     pbs = [x for x in new.dtype.names if x.startswith('F')]
+    cols = 'purple,blue,green,red,orange,black'
+    col_pbs = dict(zip(pbs, cols.split(',')))
+
+    # setup figures
     fig1 = plt.figure(figsize=(10,15))
     fig2 = plt.figure(figsize=(10,15))
     fig3 = plt.figure(figsize=(10,15))
 
-    cols = 'purple,blue,green,red,orange,black'
-    col_pbs = dict(zip(pbs, cols.split(',')))
-
+    # join the old and new photometry tables
     combined = at.join(old, new, keys='obj', table_names=['old','new'])
-    print(combined)
 
+    # create plots
     for i, pb in enumerate(pbs):
         ax1 = fig1.add_subplot(3,2,i+1)
         ax2 = fig2.add_subplot(3,2,i+1)
         ax3 = fig3.add_subplot(3,2,i+1)
 
-
+        # setup column names
         old_key = '{}_old'.format(pb)
         new_key = '{}_new'.format(pb)
         old_ekey = 'd{}_old'.format(pb)
         new_ekey = 'd{}_new'.format(pb)
 
+        # mag-mag plot
         ax1.errorbar(combined[old_key], combined[new_key], xerr=combined[old_ekey], yerr=combined[new_ekey],\
                 marker='o', linestyle='None', color=col_pbs[pb], label=pb)
         xmin, xmax = ax1.get_xlim()
@@ -47,6 +54,7 @@ def main():
         ax1.set_xlabel('Old Mag')
         ax1.set_ylabel('New Mag')
 
+        # mag-delta Mag plot
         deltaMag = combined[new_key] - combined[old_key]
         ax2.errorbar(combined[old_key], deltaMag,\
                 xerr=combined[old_ekey], yerr = combined[new_ekey],\
@@ -55,6 +63,7 @@ def main():
         mean_deltaMag = deltaMag.mean()
         std_deltaMag  = deltaMag.std()
 
+        # label the outliers
         for label, x, y in zip(combined['obj'], combined[old_key], deltaMag):
             if np.abs(y - mean_deltaMag) > 2.5*std_deltaMag:
                 ax2.annotate(label, (x, y), rotation=90)
@@ -62,11 +71,13 @@ def main():
         ax2.set_xlabel('Old Mag')
         ax2.set_ylabel('Delta (New - Old) Mag')
 
-        hist(combined[new_key] - combined[old_key], histtype='stepfilled', bins='scott', facecolor=col_pbs[pb], label=pb, ax=ax3)
+        # histograms of differences
+        ax3label = 'N({:+0.3f}, {:+0.3f})'.format(mean_deltaMag, std_deltaMag)
+        hist(combined[new_key] - combined[old_key], histtype='stepfilled', bins='scott', facecolor=col_pbs[pb], label=ax3label, ax=ax3)
         ax3.axvline(0., linestyle='--', color='black', alpha=0.5)
         ax3.set_xlabel('{} New - Old'.format(pb))
+        ax3.legend()
 
-    plt.tight_layout()
     with PdfPages('AC_AS_apparent_phot_comparison.pdf') as pdf:
         fig1.suptitle('Mag Mag comparison')
         pdf.attach_note('Mag Mag comparison')
