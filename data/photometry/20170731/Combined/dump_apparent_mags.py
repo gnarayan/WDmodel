1a724a751935b71b54d29382b63a8e256e91f420
Gautham Narayan <gsnarayan@gmail.com>
2017-09-05 13:50:04 -0400

    fix column names in zeropoint file, and update dump script to use astropy tables, avoiding python bytestring fuckery

--- data/photometry/20170731/Combined/dump_apparent_mags.py
+++ data/photometry/20170731/Combined/dump_apparent_mags.py
@@ -8,40 +8,38 @@ name_map.dat, and applying the zeropoints in ZP_all
 
 import sys
 import numpy as np
+import astropy.table as at
 from matplotlib.mlab import rec2txt
 from uncertainties import ufloat
 
 def main():
-    a = np.recfromtxt('WD_cycle22_cycle20new.phot', names=True)
+    a = at.Table.read('WD_cycle22_cycle20new.phot', format='ascii.commented_header')
     pb = 'F275W,F336W,F475W,F625W,F775W,F160W'
     pb = pb.split(',')
 
-    b = np.recfromtxt('ZP_all', names='l,ZP_10_2017,ZP_10,eZP_10,ZP_inf,eZP_inf')
-    b.l *= 1000
-    zppb = ['F{:.3}W'.format(str(x)) for x in b.l]
+    b = at.Table.read('ZP_all', format='ascii.commented_header')
+    b['l'] *= 1000
+    zppb = ['F{:.3}W'.format(str(x)) for x in b['l']]
 
-    zp = {x:y for x, y in zip(zppb, b.ZP_10)}
-    zp.update({'d'+x:y for x, y in zip(zppb, b.eZP_10)})
+    zp = {x:y for x, y in zip(zppb, b['ZP_10'])}
+    zp.update({'d'+x:y for x, y in zip(zppb, b['eZP_10'])})
 
     map = None
     with open('name_map.dat','r') as f:
         lines = f.readlines()
         map = {line.strip().split()[0]:line.strip().split()[1] for line in lines}
 
-    d = a[1:]
+    d = a[0:]
     out = []
     for i, x in enumerate(d):
         this_out = []
-        objid = str(x.ID)
+        objid = str(x['ID'])
         objid = objid.replace('-','',1).split('.')[0].split('+')[0].split('-')[0].replace('SDSS','sdss').lower()
         objid =  map.get(objid, objid)
         this_out.append(objid)
         for p in pb:
             inst_mag = x[p]
-            if p == 'F160W':
-                dinst_mag = x['d'+p]
-            else:
-                dinst_mag = x['d'+p+'1']
+            dinst_mag = x['d'+p+'1']
 
             z = zp[p]
             dz = zp['d'+p]
@@ -55,9 +53,8 @@ def main():
     for p in pb:
         names.append(p)
         names.append('d'+p)
-    out = np.rec.fromrecords(out, names=names)
-    with open('WDphot_C22_AC_AS_combined.dat','w') as f:
-        f.write(rec2txt(out, precision=5)+'\n')
+    out = at.Table(rows=out, names=names)
+    out.write('WDphot_C22_AC_AS_combined.dat',format='ascii.commented_header', overwrite=True)
 
 
 
