cc4032af081d3004524d237c766c3e16eb0a4fed
Gautham Narayan <gsnarayan@gmail.com>
2018-04-26 21:37:23 -0400

    add custom reddening model to testing suite

--- test.sh
+++ test.sh
@@ -11,12 +11,12 @@ fi
 
 mpirun -np 4 $RUNNER -m WDmodel --specfile WDmodel/tests/test.flm --photfile WDmodel/tests/test.phot --rebin 2 --everyn 3 --mpi --redo --trimspec 3700 5200 --samptype pt --ntemps 2 --nburnin 20 --nprod 100 --nwalkers 100 --covtype SHO --tau_fix True --tau 5000 --lamshift 2 --vel -50 --reddeningmodel f99 --excludepb F160W --rescale --blotch --phot_dispersion 0.001 --thin 2 --skipminuit --dl 450 --trimspec 3700 5200
 mpirun -np 4 $RUNNER -m WDmodel --specfile WDmodel/tests/test.flm --resume --nprod 5 --mpil
-mpirun -np 4 $RUNNER -m WDmodel --specfile WDmodel/tests/test.flm --ignorephot --rebin 2 --everyn 3 --nburnin 10 --nprod 50 --mpi --redo --nwalkers 100
+mpirun -np 4 $RUNNER -m WDmodel --specfile WDmodel/tests/test.flm --ignorephot --rebin 2 --everyn 3 --nburnin 10 --nprod 50 --mpi --redo --nwalkers 100 --reddeningmodel od94
 if [ "$1" = -c ]; then
     coverage combine
 fi
 $ARUNNER -m WDmodel --specfile WDmodel/tests/test.flm --ignorephot --nburnin 10 --nprod 50 --redo --outdir out_temp --covtype Exp --nwalkers 50 --av_fix True --av 0.03 --savefig --mu None --samptype gibbs --reddeningmodel ccm89
-$ARUNNER -m WDmodel --specfile WDmodel/tests/test.flm --ignorephot --nprod 50 --nburnin 10 --redo --nwalkers 30 --covtype White
+$ARUNNER -m WDmodel --specfile WDmodel/tests/test.flm --ignorephot --nprod 50 --nburnin 10 --redo --nwalkers 30 --covtype White --reddeningmodel custom
 $ARUNNER test_WDmodel.py
 
 if [ "$1" = -c ]; then
